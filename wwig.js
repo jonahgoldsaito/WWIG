18934ed1b22dd8ac6fd4c6de5fb3a1acd1b7a85b
Jonah Goldstein <jazgold@gmail.com>
2012-04-19 12:11:07 -0400

    stats update

--- wwig.js
+++ wwig.js
@@ -13,7 +13,7 @@
         last_height = $stream.outerHeight();
     
     
-    $('body').append("<style>#wwig { position:absolute; top: -13px; left: -2px; width: 154px; z-index: 5;} .profile_card .profile_picture { position relative; top:0; left:0; } .stats li.new_stats{ border-top: 1px solid #E8E8E8; display: block; padding: 6px 12px 6px 0; color: #999; font-size: 10px; line-height: 16px; text-transform: uppercase; } .new_stats strong { color: #333; font-size: 14px; } #wwig-wwig-per-day strong{ padding: 2px 4px; border-radius: 3px; background: #EEE;}</style>");
+    $('body').append("<style>#wwig { position:absolute; top: -13px; left: -2px; width: 154px; z-index: 5;} .profile_card .profile_picture { position relative; top:0; left:0; } .stats li.new_stats{ border-top: 1px solid #E8E8E8; display: block; padding: 6px 12px 6px 0; color: #999; font-size: 10px; line-height: 16px; text-transform: uppercase; } .new_stats strong { color: #333; font-size: 14px; } #wwig-wwig-per-day strong{ padding: 2px 4px; border-radius: 3px; background: #856C48; color: #FDE8C4; text-shadow: 0 1px#725B3B; }</style>");
     
     function differentiate_replies(){
       $stream.find(".stream-item").each( function( idx, el ){
@@ -69,14 +69,19 @@
 		    var $tpd = $('#wwig-tweets-per-day'),
 		        $wpd = $('#wwig-wwig-per-day');
 		    if ( $tpd.length==0 ){
-          $tpd = $('<li id="wwig-tweets-per-day" class="new_stats"><strong>'+tweets_per_day+'</strong> Tweets / Day</li>')
+          $tpd = $('<li id="wwig-tweets-per-day" class="new_stats" data-msg="An estimated number of tweets per day"><strong>'+tweets_per_day+'</strong> Tweets / Day</li>')
                   .appendTo(('.js-mini-profile-stats')).hide().slideDown();
-          $wpd = $('<li id="wwig-wwig-per-day" class="new_stats"><strong>'+wwig_per_day+'</strong> WWIG / Day</li>')
+          $wpd = $('<li id="wwig-wwig-per-day" class="new_stats" data-msg="What you would get per day (not including @ tweets to other folks)"><strong>'+wwig_per_day+'</strong> WWIG / Day</li>')
                   .appendTo(('.js-mini-profile-stats')).hide().slideDown();
           
 		    }
 		    $tpd.find('strong').html(tweets_per_day);
 		    $wpd.find('strong').html(wwig_per_day);
+		    
+		    $("li.new_stats").click(function(){
+		      var msg = $(this).attr('data-msg');
+		      if ( msg ) alert( msg );
+		    });
 		    
 		    var $wwig = $('#wwig'),
 		        img = "//github.com/jonahgoldstein/WWIG/raw/master/wwig.png";
